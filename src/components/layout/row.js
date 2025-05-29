af639a9884dfdc5808147cb077bbc94e70cf5699
Kelly Dwan <ryelle@users.noreply.github.com>
2025-05-29 17:22:09 -0400

    Components: Pass through style property for Row

--- src/components/layout/row.js
+++ src/components/layout/row.js
@@ -1,8 +1,12 @@
 // Internal dependencies
 import css from "./style.module.css";
 
-function Row({ children }) {
-  return <div className={css.row}>{children}</div>;
+function Row({ children, style }) {
+  return (
+    <div className={css.row} style={style}>
+      {children}
+    </div>
+  );
 }
 
 export default Row;
