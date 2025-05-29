781b8214a6c912e3afdfb9761ff3861264c192e3
Kelly Dwan <ryelle@users.noreply.github.com>
2025-05-29 15:06:26 -0400

    Form: Wire up the quantity inputs and total cost

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,3 +1,6 @@
+// External dependencies
+import { useState } from "react";
+
 // Internal dependencies
 import { displayPrice } from "../../utils/currency";
 import InputControl from "../input-control";
@@ -5,13 +8,27 @@ import Row from "../layout/row";
 import { useBand } from "../../data/context";
 
 function Form() {
+  const band = useBand();
+  const [tickets, setTickets] = useState(
+    band.ticketTypes.map((ticket) => ({ ...ticket, quantity: 0 }))
+  );
+  const totalCost = tickets.reduce((acc, ticket) => {
+    if (ticket.quantity > 0) {
+      acc += ticket.quantity * ticket.cost;
+    }
+    return acc;
+  }, 0);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("form data");
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Select tickets</h2>
       {/* eslint-disable-next-line jsx-a11y/no-redundant-roles -- Needed since the list style is removed. */}
       <ul role="list">
-        {band.ticketTypes.map((ticket) => (
+        {tickets.map((ticket) => (
           <li key={ticket.type}>
             <Row>
               <div>
@@ -20,13 +37,27 @@ function Form() {
                 <p>{displayPrice(ticket.cost)}</p>
               </div>
               <InputControl
-                label="Quantity"
+                label={`${ticket.name} quantity`}
                 hideLabelFromVision
                 name={ticket.type}
                 type="number"
                 min="0"
                 max="10"
                 value={ticket.quantity}
+                onChange={(value) => {
+                  // @todo Could move this validation into InputControl, using min/max to clamp the value.
+                  value = Number(value);
+                  if (value < 0) {
+                    value = 0;
+                  }
+                  const updatedTickets = tickets.map((_ticket) => {
+                    if (_ticket.type === ticket.type) {
+                      return { ..._ticket, quantity: value };
+                    }
+                    return _ticket;
+                  });
+                  setTickets(updatedTickets);
+                }}
               />
             </Row>
           </li>
@@ -35,7 +66,7 @@ function Form() {
       <div>
         <Row>
           <h2>Total</h2>
-          <div>{100}</div>
+          <div style={{ textAlign: "right" }}>{displayPrice(totalCost)}</div>
         </Row>
         <Row>
           <InputControl label="First name" />
